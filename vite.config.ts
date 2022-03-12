2430b43cc017a198a1916ec5b6782c65f6bad289
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2022-03-12 20:28:13 +0900

    Setup linter, formatter and vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  css: {
+    preprocessorOptions: {
+      sass: { charset: false },
+      scss: { charset: false },
+    },
+  },
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+  },
 })
