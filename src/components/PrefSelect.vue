2232a07961b967bda67be817e8abee0d8b631ec4
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2022-03-14 03:01:46 +0900

    [WIP] 都道府県選択ボックスコンポーネントにAPI機能を追加

--- src/components/PrefSelect.vue
+++ src/components/PrefSelect.vue
@@ -1,38 +1,65 @@
 <script setup lang="ts">
   import { ref } from 'vue'
-  import { getPrefectureData } from '../module/resasApi'
+  import { getPopulationData, getPrefectureData } from '../module/resasApi'
+  import { usePrefStore } from '../store'
 
   // 都道府県名データのインターフェース
-  interface PrefResult {
-    prefCode: string
+  interface Prefecture {
+    prefCode: number
     prefName: string
+    isChecked?: boolean
   }
 
-  const prefectures = ref<Array<PrefResult>>()
-  const checkedPrefs = ref([]) // 選択された都道府県のprefCodeを格納
-  const getData = async () => {
-    const res = await getPrefectureData<Array<PrefResult>>()
-    if (typeof res === 'string') {
-      return res
-    }
-    prefectures.value = res.result
+  // カテゴリー（例: 総人口）ごとのデータ
+  interface CategoryData {
+    label: string
+    data: Array<{ year: number; value: number }>
   }
-  getData()
+
+  // 結果を表すインターフェース
+  interface PopulationResult {
+    boundaryYear: number
+    data: Array<CategoryData>
+  }
+
+  const prefStore = usePrefStore()
+
+  const prefectures = ref<Array<Prefecture>>()
+
+  const getPrefData = async () => {
+    const res = await getPrefectureData<Array<Prefecture>>()
+    if (typeof res === 'string') {
+      alert('データが取得できませんでした。')
+    } else {
+      prefectures.value = res.result
+    }
+  }
+
+  const prefChange = async (pref: Prefecture) => {
+    pref.isChecked = pref.isChecked ? false : true
+    if (pref.isChecked) {
+      const res = await getPopulationData<PopulationResult>(pref.prefCode)
+      if (typeof res === 'string') {
+        alert('データが取得できませんでした。')
+      } else {
+        prefStore.set(pref.prefCode, pref.prefName, res.result.data[0].data)
+      }
+    } else {
+      prefStore.remove(pref.prefCode)
+    }
+  }
+
+  getPrefData()
 </script>
 
 <template>
   <section>
     <h2>都道府県</h2>
-    <p v-if="!prefectures">データが取得できませんでした。</p>
-    <div>Checked prefs: {{ checkedPrefs }}</div>
     <div v-for="p in prefectures" :key="p.prefCode">
-      <input
-        type="checkbox"
-        :id="p.prefCode"
-        :value="p.prefCode"
-        v-model="checkedPrefs"
-      />
-      <label :for="p.prefCode" data-testid="prefecture">{{ p.prefName }}</label>
+      <input type="checkbox" :id="`${p.prefCode}`" @change="prefChange(p)" />
+      <label :for="`${p.prefCode}`" data-testid="prefecture">{{
+        p.prefName
+      }}</label>
     </div>
   </section>
 </template>
