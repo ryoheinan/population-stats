5cfa8b2830153064c16e6f9ce2bde36f4082ed6e
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2022-03-14 03:02:13 +0900

    [WIP] グラフ表示機能を実装

---
+++ src/components/PopulationChart.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import { computed } from 'vue'
+  import { usePrefStore } from '../store'
+  import Chart from './Chart.vue'
+
+  const prefStore = usePrefStore()
+
+  const dataset = computed(() => Object.values(prefStore.prefData))
+</script>
+
+<template>
+  <section>
+    <h2>グラフ</h2>
+    <p v-if="dataset.length === 0">データが取得できませんでした。</p>
+    <Chart v-else title="test" :dataset="dataset" />
+  </section>
+</template>
