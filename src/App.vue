73091075942874a9bb35a327aab2f6fd7e7f8833
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2022-03-14 14:29:22 +0900

    [WIP] PopulationChartコンポーネントをAppに追加

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
   import PrefSelect from './components/PrefSelect.vue'
   import Header from './components/Header.vue'
+  import PopulationChart from './components/PopulationChart.vue'
 </script>
 
 <template>
   <Header />
-  <main><PrefSelect /></main>
+  <main><PrefSelect /> <PopulationChart /></main>
 </template>
 
 <style lang="scss"></style>
